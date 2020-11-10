244b0ea2c64205a85a186cde8d53b947bd4e4847
Grigoryev Nikita <nvgrigoryev@gmail.com>
2020-11-10 21:59:42 +0400

    HW1 finally done

--- src/ArrayStorage.java
+++ src/ArrayStorage.java
@@ -27,18 +27,15 @@ public class ArrayStorage {
     }
 
     void delete(String uuid) {
-        if (size == 0) {
-            return;
-        }
         for (int i = 0; i < size; i++) {
             if (storage[i].uuid.equals(uuid)) {
                 for (int j = i; j < size - 1; j++) {
                     storage[j] = storage[j + 1];
                 }
+                size--;
                 break;
             }
         }
-        size--;
     }
 
     /**
