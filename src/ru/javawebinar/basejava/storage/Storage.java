bfea9e7706fb6abb5898dc0ddd7deba8ae06016d
Grigoryev Nikita <nvgrigoryev@gmail.com>
2020-11-21 16:01:32 +0400

    introduce interfece Storage

---
+++ src/ru/javawebinar/basejava/storage/Storage.java
@@ -0,0 +1,30 @@
+package ru.javawebinar.basejava.storage;
+
+import ru.javawebinar.basejava.model.Resume;
+
+public interface Storage {
+
+    //полная очистка storage
+    void clear();
+
+    //обновление resume, которое имеется в storage
+    void update(Resume resume);
+
+    //сохранение нового resume в storage
+    void save(Resume resume);
+
+    //получение resume из storage
+    Resume get(String uuid);
+
+    //удаление resume из storage
+    void delete(String uuid);
+
+    //получение всех resume из storage
+    Resume[] getAll();
+
+    //получение количества resume в storage
+    int size();
+
+    //поиск позиции resume в storage
+    int findResume(String uuid);
+}
