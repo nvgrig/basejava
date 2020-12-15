76209c41ced43c44d1c843053a86f78d20bbafc8
Grigoryev Nikita <nvgrigoryev@gmail.com>
2020-12-15 21:49:23 +0400

    // TODO Reflection deleted

--- src/ru/javawebinar/basejava/MainReflection.java
+++ src/ru/javawebinar/basejava/MainReflection.java
@@ -15,7 +15,6 @@ public class MainReflection {
         System.out.println(field.getName());
         System.out.println(field.get(r));
         field.set(r, "new_uuid");
-        // TODO : invoke r.toString via reflection
         Method method = r.getClass().getDeclaredMethod("toString");
         System.out.println(method.invoke(r));
     }
