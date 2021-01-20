04b2871d97dbcf46749be758293690caed4988d7
Grigoryev Nikita <nvgrigoryev@gmail.com>
2021-01-21 00:20:03 +0400

    ObjectStreamPathStorage & ObjectStreamPathStorageTest implemented

---
+++ test/ru/javawebinar/basejava/storage/ObjectStreamPathStorageTest.java
@@ -0,0 +1,9 @@
+package ru.javawebinar.basejava.storage;
+
+public class ObjectStreamPathStorageTest extends AbstractStorageTest {
+
+    public ObjectStreamPathStorageTest() {
+        super(new ObjectStreamStorage(STORAGE_DIR));
+    }
+
+}
