aef0925552d98c541a912298ca19b4fa39d3df77
Grigoryev Nikita <nvgrigoryev@gmail.com>
2021-02-23 18:16:46 +0400

    Deadlock example

--- src/ru/javawebinar/basejava/MainConcurrency.java
+++ src/ru/javawebinar/basejava/MainConcurrency.java
@@ -1,5 +1,7 @@
 package ru.javawebinar.basejava;
 
+import ru.javawebinar.basejava.exception.StorageException;
+
 import java.util.ArrayList;
 import java.util.List;
 
@@ -58,6 +60,20 @@ public class MainConcurrency {
             }
         });
         System.out.println(mainConcurrency.counter);
+
+        //deadlock example
+        Account account1 = new Account(1000);
+        Account account2 = new Account(1000);
+
+        new Thread(() -> {
+            for (int i = 0; i < 500; i++) {
+                Operations.transfer(account1, account2, 1);
+            }
+        }).start();
+
+        for (int i = 0; i < 500; i++) {
+            Operations.transfer(account2, account1, 1);
+        }
     }
 
     private synchronized void inc() {
@@ -68,5 +84,46 @@ public class MainConcurrency {
 //                readFile
 //                ...
 //        }
+    }
+}
+
+//classes for deadlock realization
+class Account {
+    private int balance;
+
+    public Account(int balance) {
+        this.balance = balance;
+    }
+
+    public int getBalance() {
+        return balance;
+    }
+
+    public void deposit(int amount) {
+        balance += amount;
+    }
+
+    public void withdraw(int amount) {
+        balance -= amount;
+    }
+}
+
+class Operations {
+    public static void transfer(Account account1, Account account2, int amount) throws StorageException {
+        if (account1.getBalance() < amount) {
+            throw new StorageException("Insufficient funds");
+        }
+        synchronized (account1) {
+            try {
+                Thread.sleep(1000);
+            } catch (InterruptedException e) {
+                e.printStackTrace();
+            }
+            synchronized (account2) {
+                account1.withdraw(amount);
+                account2.deposit(amount);
+            }
+        }
+        System.out.println("Transfer complete");
     }
 }
