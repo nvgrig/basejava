bf1a23809cdac38a964b2a6c0a2cb01ebde4e3e1
Grigoryev Nikita <nvgrigoryev@gmail.com>
2021-03-16 23:00:06 +0400

    init_db.sql created

---
+++ config/init_db.sql
@@ -0,0 +1,16 @@
+create table resume
+(
+    uuid      char(36) primary key NOT NULL,
+    full_name text                 NOT NULL
+);
+
+create table contact
+(
+    id          serial,
+    resume_uuid char(36) not null references resume (uuid) on delete cascade,
+    type        text     not null,
+    value       text     not null
+);
+
+create unique index contact_uuid_type_index
+    on contact (resume_uuid, type);
