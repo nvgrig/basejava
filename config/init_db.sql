1b98821f542fee0a7ffee21d943fd3d49d6ac4ab
Grigoryev Nikita <nvgrigoryev@gmail.com>
2021-04-05 22:26:33 +0400

    HW15 servlet + ddl section

--- config/init_db.sql
+++ config/init_db.sql
@@ -12,5 +12,16 @@ create table contact
     value       text     not null
 );
 
+create table section
+(
+    id          serial,
+    type        text     not null,
+    value       text     not null,
+    resume_uuid char(36) not null references resume on delete cascade
+);
+
 create unique index contact_uuid_type_index
     on contact (resume_uuid, type);
+
+create unique index section_uuid_type_index
+    on section (resume_uuid, type);
